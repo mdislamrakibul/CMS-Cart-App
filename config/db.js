8e7f43cb057c2f3d608c155efd191ee13421c647
unknown <rakibul@nexkraft.com>
2021-02-22 13:52:35 +0600

    make it better

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost/cmscart'
-    //database: 'mongodb+srv://rakibul:1234@cms-cart.2bpj7.mongodb.net/cms-cart?retryWrites=true&w=majority'
+    //database: 'mongodb://localhost/cmscart'
+    database: 'mongodb+srv://rakibul:1234@cms-cart.2bpj7.mongodb.net/cms-cart?retryWrites=true&w=majority'
 }
